cd2fe87cf06845c03c3d8bb8185aa2c015e89c5d
openjason <openjc@outlook.com>
2018-11-27 06:31:07 +0800

    1127m

--- todb.py
+++ todb.py
@@ -10,9 +10,17 @@ CREATE TABLE IF NOT EXISTS `sz300750` (
   `bors` char(8) CHARACTER SET latin1 DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 '''
-
-#grant all privileges on stock.* to jcc@'%' identified by 'password';
+#CREATE USER 'jcc'@'%' IDENTIFIED BY 'pwd123456';
+#grant all privileges on stock.* to 'jcc'@'%';
 #flush privileges;
+# #alter user 'jcc'@'%' identified with mysql_native_password by 'pwd123456'; #修改加密规则 
+
+#MYSQL图像化工具(HeidiSQL)，连接本地MYSQL时，提示“cacheing_sha2_password”的错误信息。
+# 在MYSQL 的版本问题上，MYSQL8版本以上，默认存储密码的方式修为:caching_sha2_password,而MYSQL8版本以下，默认存储密码的方式为：mysql_native_password.
+# # ALTER USER'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root'; #更新一下用户的密码 
+# FLUSH PRIVILEGES; #刷新权限
+# 重置密码：alter user'root'@'localhost' identified by 'root';
+
 #LOAD DATA LOW_PRIORITY LOCAL INFILE 'F:\\dev\\GitHub\\security\\usz300750_1121.log' REPLACE INTO TABLE `stock`.`sz300750` FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' ESCAPED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 LINES (`xdate`, `price`, `pdiff`, `volume`, `amount`, `bors`);
 
 import pymysql.cursors
