6a2ea0da50a57f8d6b8e0cc6a7cae32e03a8b7b6
openjason <openjc@outlook.com>
2019-06-18 13:44:40 +0800

    0618

--- ema.py
+++ ema.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import numpy
 import pandas as pd
 
-df = pd.DataFrame(getKline('1day','30','0'))
+df = pd.read_csv(r'F:\dev\GitHub\security\book1.csv')
+#df = pd.DataFrame(getKline('1day','30','0'))
 df.columns = ['date', 'open', 'high', 'low', 'close', 'amount']
 
 def calc_EMA(df, N): 
     for i in range(len(df)):
         if i==0:
-            df.ix[i,'ema']=df.ix[i,'close']
+            df.loc[i,'ema']=df.loc[i,'close']
         if i>0:
-            df.ix[i,'ema']=((N-1)*df.ix[i-1,'ema']+2*df.ix[i,'close'])/(N+1)
+            df.loc[i,'ema']=((N-1)*df.loc[i-1,'ema']+2*df.loc[i,'close'])/(N+1)
     ema=list(df['ema'])
     return ema
 
@@ -21,10 +22,10 @@ def calc_MACD(df, short=12, long=26, M=9):
     df['diff'] = pd.Series(emas) - pd.Series(emaq)
     for i in range(len(df)):
         if i==0:
-            df.ix[i,'dea'] = df.ix[i,'diff']  
+            df.loc[i,'dea'] = df.loc[i,'diff']  
         if i>0:  
-            df.ix[i,'dea'] = ((M-1)*df.ix[i-1,'dea'] + 2*df.ix[i,'diff'])/(M+1)  
+            df.loc[i,'dea'] = ((M-1)*df.loc[i-1,'dea'] + 2*df.loc[i,'diff'])/(M+1)  
     df['macd'] = 2*(df['diff'] - df['dea'])
     return df
 
-calc_MACD(df, 12, 26, 9)
+print(calc_MACD(df, 12, 26, 9))
