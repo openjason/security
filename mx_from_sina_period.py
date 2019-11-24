e54b71782224f7258040d5399308283ca43d3e41
openjason <openjc@outlook.com>
2019-11-25 06:50:50 +0800

    1125

--- mx_from_sina_period.py
+++ mx_from_sina_period.py
@@ -6,14 +6,14 @@ from frech_sina import his_daily_mx_from_sina
 
 if __name__ == '__main__':
 
-    curr_stock = 'sz000063'
+    curr_stock = 'sz300059'
 
-    date_begin = datetime.datetime(2019,9,7)
-    for i in range(250):
+    date_begin = datetime.datetime(2019,7,25)
+    for i in range(350):
         # Monday is 0 and Sunday is 6.
         curr_date = str(date_begin.strftime('%Y-%m-%d'))
         if date_begin.isoweekday() < 6:
-            curr_filename = curr_stock+'_'+curr_date[5:]+'.log'
+            curr_filename = curr_stock+'_'+curr_date+'.log'
             print(curr_filename)
             his_daily_mx_from_sina(curr_stock,curr_date,curr_filename)
         else:
